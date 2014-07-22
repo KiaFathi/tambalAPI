f87259dde9af22080ec14a68b173c42c9324d77c
Kia Fathi <kiafathi@me.com>
2014-07-21 21:03:57 -0700

    Add some basic jokes and change paths to reflect jokes

--- server.js
+++ server.js
@@ -4,30 +4,34 @@ var port = process.env.PORT || 8500;
 
 
 
-var quotes = [
-  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+var jokes = [
+  {joke: 'Did you hear about the guy whose whole left side was cut off? He\'s all right now.'},
+  {joke: "I'm reading a book about anti-gravity. It's impossible to put down."},
+  {joke: "I wondered why the baseball was getting bigger. Then it hit me."},
+  {joke: "It's not that the man did not know how to juggle, he just didn't have the balls to do it."},
+  {joke: "I'm glad I know sign language, it's pretty handy."},
+  {joke: "My friend's bakery burned down last night. Now his business is toast."},
+  {joke: "Why did the cookie cry? It was feeling crumby."},
+  {joke: "I used to be a banker, but I lost interest."}
 ];
 
 
 app.get('/', function(req, res){
-  res.json(quotes);
+  res.json(jokes);
 });
 
-app.get('/quote/random', function(req, res){
-  var index = Math.floor(Math.random() * quotes.length);
-  var randomQuote = quotes[index];
+app.get('/joke/random', function(req, res){
+  var index = Math.floor(Math.random() * jokes.length);
+  var randomQuote = jokes[index];
   res.json(randomQuote);
 });
 
-app.get('/quote/:id', function(req, res) {
-  if(quotes.length <= req.params.id || req.params.id < 0) {
+app.get('/joke/:id', function(req, res) {
+  if(jokes.length <= req.params.id || req.params.id < 0) {
     res.statusCode = 404;
-    return res.send('Error 404: No quote found');
+    return res.send('Error 404: No joke found');
   }  
-var q = quotes[req.params.id];
+var q = jokes[req.params.id];
   res.json(q);
 });
 
